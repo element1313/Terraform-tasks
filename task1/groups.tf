527341a29a9d0cc733a25892c9a2253487874bea
element1313 <xurhohs@gmail.com>
2020-08-22 06:26:37 +0000

    added users

--- task1/groups.tf
+++ task1/groups.tf
@@ -2,3 +2,8 @@ resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/"
 }
+
+resource "aws_iam_group" "management" {
+  name = "management"
+  path = "/"
+}
--- task1/membership.tf
+++ task1/membership.tf
@@ -3,7 +3,22 @@ resource "aws_iam_group_membership" "developers_team" {
 
   users = [
     "${aws_iam_user.Tim.name}",
+    "${aws_iam_user.Bob.name}",
+    "${aws_iam_user.Ben.name}",
   ]
 
   group = "${aws_iam_group.developers.name}"
+}
+
+
+
+
+resource "aws_iam_group_membership" "management_team" {
+  name = "management-group-membership"
+
+  users = [
+    "${aws_iam_user.Ben.name}",
+  ]
+
+  group = "${aws_iam_group.management.name}"
 }
