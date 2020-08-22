8bc83196e1d5a164f84608f6db4e6fdfc87a01cc
element1313 <xurhohs@gmail.com>
2020-08-22 05:23:47 +0000

    added new files

--- task1/provider.tf
+++ task1/provider.tf
@@ -1,20 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_iam_user" "Tim" {
-  name = "Tim"
-}
-
-resource "aws_iam_group" "developers" {
-  name = "developers"
-  path = "/"
-}
-
-resource "aws_iam_group_membership" "developers_team" {
-  name = "developers-group-membership"
-  users = [
-    "${aws_iam_user.Tim.name}"
-  ]
-  group = "${aws_iam_group.developers.name}"
-}
