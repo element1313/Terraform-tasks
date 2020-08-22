5b38712f55c9f56a69f670ad27b143e7f15ec79b
element1313 <xurhohs@gmail.com>
2020-08-22 05:06:23 +0000

    added group

--- task1/provider.tf
+++ task1/provider.tf
@@ -10,3 +10,11 @@ resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/"
 }
+
+resource "aws_iam_group_membership" "developers_team" {
+  name = "developers-group-membership"
+  users = [
+    "${aws_iam_user.Tim.name}"
+  ]
+  group = "${aws_iam_group.developers.name}"
+}
