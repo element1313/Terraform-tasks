29c5f90ac111ff91e2ce390f05c85929c616fd24
element1313 <xurhohs@gmail.com>
2020-08-22 05:57:03 +0000

    added classes

---
+++ task1/S3.tf
@@ -0,0 +1,5 @@
+# resource "aws_s3_bucket" "b" {
+#   bucket = "my-tf-test-bucket-shokhrukh"
+#   region = "ap-southeast-1"
+ 
+# }
--- task1/membership.tf
+++ task1/membership.tf
@@ -1,7 +1,9 @@
 resource "aws_iam_group_membership" "developers_team" {
   name = "developers-group-membership"
+
   users = [
-    "${aws_iam_user.Tim.name}"
+    "${aws_iam_user.Tim.name}",
   ]
+
   group = "${aws_iam_group.developers.name}"
 }
