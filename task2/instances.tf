359ffdde40f760722d30fb61e12a4846561ea9dc
element1313 <xurhohs@gmail.com>
2020-08-23 03:41:43 +0000

    added sec group

--- task2/instances.tf
+++ task2/instances.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
+    #count = 10
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t2.micro"
-}
-
-resource "aws_key_pair" "task" {
-  key_name   = "task-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  associate_public_ip_address = "true"
+  key_name      = "${aws_key_pair.task.key_name}"
+  user_data     = "${file("userdata.sh")}"
+  vpc_security_group_ids  = ["${aws_security_group.allow_ssh.id}"]
 }
